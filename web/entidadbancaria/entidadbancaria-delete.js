f963f7b17db90bc0408b9ba4584c66bac3fc8589
alumno <alumno@2A5PC24>
2015-10-30 10:13:14 +0100

    ROUTES ADDED NEW FILES STRUCTURE ADDED ANGULARJS SERVICES

--- web/entidadbancaria/entidadbancaria-delete.js
+++ web/entidadbancaria/entidadbancaria-delete.js
@@ -1,12 +1,24 @@
-app.controller("DeleteController",DeleteController);
-function DeleteController($scope,entidadBancariaService,$routeParams){
-var response;
-response = entidadBancariaService.delete($routeParams.idEntidadBancaria);
-
-      response.success(function(data, status, headers, config) {
-     $scope.mensaje = "Borrado OK";
-  }).error(function(data, status, headers, config) {
-      alert("Ha fallado la petición. Estado HTTP:"+status);
-  });
-}
 DeleteController.$inject = ["$scope", "entidadBancariaService", "$routeParams"];
+function DeleteController($scope, entidadBancariaService, $routeParams) {
+
+     entidadBancariaService.get($routeParams.idEntidadBancaria).success(function (data, status, headers, config) {
+        $scope.entidadBancaria = data;
+    }).error(function (data, status, headers, config) {
+        alert("Ha fallado la petición. Estado HTTP:" + status);
+    });
+
+
+    $scope.delete = function () {
+        var response;
+        response = entidadBancariaService.delete($routeParams.idEntidadBancaria);
+
+        response.success(function (data, status, headers, config) {
+            $scope.mensaje = "Borrado OK";
+        }).error(function (data, status, headers, config) {
+            alert("Ha fallado la petición. Estado HTTP:" + status);
+        });
+    };
+
+}
+app.controller("DeleteController", DeleteController);
+
